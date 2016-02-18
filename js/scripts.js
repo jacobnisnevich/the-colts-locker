7261611773a798e96830176ce0c0e606ba5a23f6
Jacob Nisnevich <jacob.nisnevich@gmail.com>
2016-02-18 00:41:07 -0800

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+	articles.forEach(function(article) {
+		$(".articles").append(createArticle(article.id, 
+											article.title,
+											article.author,
+											article.authorTwitter,
+											article.date, 
+											article.blurb, 
+											article.article, 
+											article.tags));
+	});
+
+	$(".article").click(function() {
+		$(this).children(".article-body").slideToggle();
+	});
+
+	$(".articles").css({
+		"min-height": $(window).height() - $(".footer").height() - 
+										   $(".header").height() - 
+										   parseInt($(".articles").css("padding"))
+	});
+});
+
+var createArticle = function(id, title, author, authorTwitter, date, blurb, article, tags) {
+	var articleDiv = "<div id='" + id + "' class='article shadow'>";
+	articleDiv += "<div class='article-header'>" + title + "</div>";
+	articleDiv += "<div class='article-author-date clearfix'>";
+	articleDiv += "<div class='article-author'><a href='" + authorTwitter + "'>" + author + "</a></div>";
+	articleDiv += "<div class='article-date'>" + date + "</div></div>";
+	articleDiv += "<div class='article-body'>" + article + "</div>";
+	articleDiv += "</div>";
+
+	return articleDiv
+}
